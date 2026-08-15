e190575aad95c68c5404c8feec9e45ed5aa73376
Shake Light <140467108+Shake-Light-CN@users.noreply.github.com>
2026-08-15 09:58:21 +0800

    Update src/config/musicConfig.ts (via Pages CMS)

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,10 +48,10 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				name: "鹿 be free",
+				artist: "尚雯婕",
+				url: "/assets/music/lubefree.mp3",
+				cover: "/assets/music/cover/lubefree.webp",
 				lrc: "",
 			},
 		],

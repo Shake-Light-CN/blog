994b7784a694cccd2c6a8c0b2386551d7b53abec
Shake Light <140467108+Shake-Light-CN@users.noreply.github.com>
2026-08-15 10:34:28 +0800

    Update src/config/booknavConfig.ts (via Pages CMS)

--- src/config/booknavConfig.ts
+++ src/config/booknavConfig.ts
@@ -27,7 +27,7 @@ export const booknavConfig: BooknavGroup[] = [
   	{
 		id: "teach",
 		name: "教育",
-		icon: "aterial-symbols:code-rounded",
+		icon: "material-symbols:code-rounded",
 		desc: "教育相关网站",
 		weight: 90,
 		items: [
